4505d8003c61b718d9d8f5f9607d33ab7fc15f55
Nhã Nguyễn Phong <nhanguyen2004@gmail.com>
2026-02-03 21:50:38 +0700

    remove description

--- run.ps1
+++
@@ -1,5 +0,0 @@
-docker-compose up -d --build
-Write-Host "System is starting..."
-Write-Host "MinIO Console: http://localhost:9001"
-Write-Host "Backend API: http://localhost:8080"
-Write-Host "Test Page: Open index.html in your browser"
